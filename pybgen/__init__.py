f3ef8983eb834b172313536a195e5e80d79ca3a8
roshchupkin <g.roshchupkin@erasmusmc.nl>
2017-11-06 17:40:58 +0100

    bgi and IDEN

--- pybgen/__init__.py
+++ pybgen/__init__.py
@@ -375,7 +375,7 @@ class Bgen(object):
                         _.append(Bgen_probe(f, self.compression, self.N_ind, layout=self.layout))
                     return _
                 else:
-                    self.seek(f, probes.file_start_position)
+                    self.seek(f, probes.file_start_position.tolist()[0])
                     probe = Bgen_probe(f, self.compression, self.N_ind, layout=self.layout)
 
         else:
